7f22c137fd449d6ce9d366e13628dda94c231e5b
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-03-29 17:35:17 +0900

    add ABC048A

---
+++ ABC048A.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# A - AtCoder *** Contest
+# https://atcoder.jp/contests/abc048/tasks/abc048_a
+
+s = list(input().title().split())
+ans = s[0][0] + s[1][0] + s[2][0]
+print(ans)
