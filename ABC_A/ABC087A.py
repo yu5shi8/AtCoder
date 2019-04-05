343d756b7dd968e6c04e0520ba5331a66139c8f2
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-05 20:38:14 +0900

    add ABC087A

---
+++ ABC_A/ABC087A.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# A - Buying Sweets
+# https://atcoder.jp/contests/abc087/tasks/abc087_a
+
+x = int(input())
+a = int(input())
+b = int(input())
+ans = (x - a) % b
+print(ans)
