6811e8a172c47a5eea007c0d70f5e255697a4c0a
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-05 23:59:42 +0900

    add ABC093A

---
+++ ABC_A/ABC093A.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# A - abc of ABC
+# https://atcoder.jp/contests/abc093/tasks/abc093_a
+
+abc = sorted(input())
+
+if abc[0] == 'a' and abc[1] == 'b' and abc[2] == 'c':
+    print('Yes')
+else:
+    print('No')
