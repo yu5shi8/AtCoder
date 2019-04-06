f0815367fd7c606f7c2247e470ecb522a3285769
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-06 16:30:34 +0900

    add ABC100A

---
+++ ABC_A/ABC100A.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# A - Happy Birthday!
+# https://atcoder.jp/contests/abc100/tasks/abc100_a
+
+a, b = map(int, input().split())
+
+if a <= 8 and b <= 8:
+    print('Yay!')
+else:
+    print(':(')
