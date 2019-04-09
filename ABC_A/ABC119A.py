a8c538af29dfba9ef4bcd6606450779782e44091
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-09 20:41:16 +0900

    add ABC119A

---
+++ ABC_A/ABC119A.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# A - Still TBD
+# https://atcoder.jp/contests/abc119/tasks/abc119_a
+
+s = input().replace('/', '')
+
+if int(s) <= 20190430:
+    print('Heisei')
+else:
+    print('TBD')
