d88c4a759134d891591236376e879c46fd6be3d2
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-09 19:51:07 +0900

    add ABC118A

---
+++ ABC_A/ABC118A.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# A - B +/- A
+# https://atcoder.jp/contests/abc118/tasks/abc118_a
+
+a, b = map(int, input().split())
+
+if b % a == 0:
+    print(a + b)
+else:
+    print(b - a)
