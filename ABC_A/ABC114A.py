fd1e152ee4ff3a62da7a3ba80a1406defb1fe9f9
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-09 19:28:22 +0900

    add ABC114A

---
+++ ABC_A/ABC114A.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# A - 753
+# https://atcoder.jp/contests/abc114/tasks/abc114_a
+
+# 19:23
+
+x = int(input())
+if x == 7 or x == 5 or x == 3:
+    print('YES')
+else:
+    print('NO')
+
+# スマートに書く方法がある
+# if x in [7, 5, 3]:
