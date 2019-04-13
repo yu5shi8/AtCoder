5a82af39cdf6d90ba4d7da381df061abc5848808
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-13 23:37:20 +0900

    add ABC124B

---
+++ ABC_B/ABC124B.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# B - Great Ocean View
+# https://atcoder.jp/contests/abc124/tasks/abc124_b
+
+n = int(input())
+h = list(map(int, input().split()))
+ans = 1
+
+for i in range(1, n):
+    if max(h[:i]) <= h[i]:
+        ans += 1
+
+print(ans)
