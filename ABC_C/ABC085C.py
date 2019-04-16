f89f421d612e4bfc206ddb0955fc08e2b4595221
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-16 18:53:22 +0900

    add ABC085C

---
+++ ABC_C/ABC085C.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# ABC085C - Otoshidama
+# https://atcoder.jp/contests/abs/tasks/abc085_c
+
+n, y = map(int, input().split())
+
+for i in range(n+1):
+    for j in range(n-i+1):
+        if i * 10000 + j * 5000 + (n-i-j)* 1000 == y:
+            print(i, j, n-i-j)
+            exit()
+
+print(-1, -1, -1)
+
+# 3つ中、2つが確定すれば残り1つも確定する
