203452da2db0b9474db0c19ff9227215943bce6c
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-17 11:11:37 +0900

    add ABC049C

---
+++ ABC_C/ABC049C.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# ABC049C - 白昼夢 / Daydream
+# https://atcoder.jp/contests/abs/tasks/arc065_a
+
+s = input().replace('eraser', '').replace('erase', '').replace('dreamer', '').replace('dream', '')
+
+if s:
+    print('NO')
+else:
+    print('YES')
