be32cc560b0b9614218f0cf9993cf5557667e527
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-20 13:43:31 +0900

    add ABC100B

---
+++ ABC_B/ABC100B.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# B - Ringo's Favorite Numbers
+# https://atcoder.jp/contests/abc100/tasks/abc100_b
+
+d, n = map(int, input().split())
+ans = 100**d * (n+n//100)
+print(ans)
+
+# 13:17 - 13:42
