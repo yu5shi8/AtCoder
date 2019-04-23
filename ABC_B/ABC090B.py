ef94bbbe569e57d8097add608db1861c2629ccb7
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-24 06:38:25 +0900

    add ABC090B

---
+++ ABC_B/ABC090B.py
@@ -0,0 +1,13 @@
+# B - Palindromic Numbers
+# https://atcoder.jp/contests/abc090/tasks/abc090_b
+
+a, b = map(int, input().split())
+count = 0
+
+for i in range(a, b+1):
+    if str(i)[0]==str(i)[4] and str(i)[1]==str(i)[3]:
+        count += 1
+
+print(count)
+
+# 6:16 - 6:36
