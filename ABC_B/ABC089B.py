050fefdfb29ef5f53e4dc843030b27a7b7c9785d
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-24 06:49:30 +0900

    add ABC089B

---
+++ ABC_B/ABC089B.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# B - Hina Arare
+# https://atcoder.jp/contests/abc089/tasks/abc089_b
+
+n = int(input())
+s = list(input().split())
+
+if s.count('Y') > 0:
+    print('Four')
+else:
+    print('Three')
+
+# 6:41 - 6:48
