53c6c35ef9a1610226dd8ad9a584a790aeefa761
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-04-27 22:55:42 +0900

    add ABC125A

---
+++ ABC_A/ABC125A.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# A - Biscuit Generator
+# https://atcoder.jp/contests/abc125/tasks/abc125_a
+
+a, b, t = map(int, input().split())
+num = t // a
+print(b * num)
+
+# 21:00 - 21:09
