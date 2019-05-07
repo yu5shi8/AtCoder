07c2abb108afcee0f750c4d253a517f004105ca1
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-07 13:20:34 +0900

    add ABC064B

---
+++ ABC_B/ABC064B.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# B - Traveling AtCoDeer Problem
+# https://atcoder.jp/contests/abc064/tasks/abc064_b
+
+n = int(input())
+a = list(map(int, input().split()))
+ans = max(a)-min(a)
+print(ans)
+
+# 13:15 - 13:19
