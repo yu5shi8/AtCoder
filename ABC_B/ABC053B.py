5d77a4877dba9b3416ae9ff65f769a0579a9cf4d
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-08 22:40:01 +0900

    add ABC053B

---
+++ ABC_B/ABC053B.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# B - A to Z String
+# https://atcoder.jp/contests/abc053/tasks/abc053_b
+
+s = input()
+a = s.find('A')
+z = s.rfind('Z') + 1
+print(z - a)
+
+# 22:32 - 22:39
