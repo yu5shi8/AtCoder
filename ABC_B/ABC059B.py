81d6e7279ec5ecfa3c8b0ff7b7a1600b9d44cc63
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-08 13:56:58 +0900

    add ABC059B

---
+++ ABC_B/ABC059B.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# B - Comparison
+# https://atcoder.jp/contests/abc059/tasks/abc059_b
+
+a = int(input())
+b = int(input())
+
+if a > b:
+    print('GREATER')
+elif a < b:
+    print('LESS')
+else:
+    print('EQUAL')
+
+# 13:51 - 13:55
