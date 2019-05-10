fe47d920dd1e9642cb2ee5ddd36742ff1b4ec70c
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-10 21:41:26 +0900

    fix ABC125C

--- ABC_C/ABC125C.py
+++ ABC_C/ABC125C.py
@@ -2,34 +2,51 @@
 # C - GCD on Blackboard
 # https://atcoder.jp/contests/abc125/tasks/abc125_c
 
-# 時間切れ
+def gcd(x, y):
+    while y != 0:
+        x, y = y, x%y
+    return x
 
 n = int(input())
 a = list(map(int, input().split()))
-count = []
-sorted(count, reverse=True)
-ans = []
+
+left = [0 for _ in range(n)]
+right = [0 for _ in range(n)]
+ans = [0 for _ in range(n)]
+
+for i in range(1, n):
+    left[i] = gcd(left[i-1], a[i-1])
+
+for i in range(n-2, -1, -1):
+    right[i] = gcd(right[i+1], a[i+1])
 
 for i in range(n):
-    print('---')
-    print(a[i])
-    for j in range(1, 100):
-        print(j)
-        if a[i] % j == 0:
-            count.append(a[i] // j)
+    ans[i] = gcd(left[i], right[i])
 
-count = sorted(count, reverse=True)
-print(count)
-
-print('---\nここから最大公約数の検討')
-
-for i in count:
-    print(i)
-    if count(i-1) == count(i):
-        ans.append(i)
-
-print(ans)
 print(max(ans))
 
 
-# 21:25 - 22:18
+
+'''
+n = int(input())
+a = list(map(int, input().split()))
+calc = []
+ans = 0
+
+for j in range(n):
+    for i in range(1, max(a)):
+        if a[j] % i == 0:
+            calc.append(i)
+
+calc = sorted(calc)
+
+for i in range(len(calc)):
+    if calc.count(calc[i]) == n-1 and ans < calc[i]:
+        ans = calc[i]
+
+print(ans)
+'''
+
+# 18:36 - 19:36（TLE）
+# （解答を閲覧）- 19:45
+# 21:21 - 21:40
