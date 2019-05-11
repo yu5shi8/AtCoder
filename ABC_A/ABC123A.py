cd9a51d320b2dfd55930e0cf17c71a8af62742b5
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-11 18:17:25 +0900

    fix ABC123A

--- ABC_A/ABC123A.py
+++ ABC_A/ABC123A.py
@@ -9,23 +9,11 @@ d = int(input())
 e = int(input())
 k = int(input())
 
-ab = abs(a - b)
-ac = abs(a - c)
-ad = abs(a - d)
-ae = abs(a - e)
-bc = abs(b - c)
-bd = abs(b - d)
-be = abs(b - e)
-cd = abs(c - d)
-de = abs(d - e)
-
-if ab > k or ac > k or ad > k or ae > k or bc > k or bd > k or be > k or cd > k or de > k:
-    print(':(')
-else:
+if e - a <= k:
     print('Yay!')
+else:
+    print(':(')
 
-# 総当たりしなくても、A〜Eの距離とKを調べればOKだった…
-# if abs(e - a) > k:
-#     print(':(')
-# else:
-#     print('Yay!')
+
+# 18:10 - 18:13（WA）
+# 18:16（AC）
