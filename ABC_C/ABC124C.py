7bf8d55b365671d0fa132660ec80d1c66533e454
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-12 11:43:16 +0900

    fix ABC124C

--- ABC_C/ABC124C.py
+++ ABC_C/ABC124C.py
@@ -2,14 +2,13 @@
 # C - Coloring Colorfully
 # https://atcoder.jp/contests/abc124/tasks/abc124_c
 
-s = list(map(int, input()))
+s = input()
 ans = 0
 
 for i in range(len(s)):
-    if i % 2 == s[i]:
+    if i % 2 == int(s[i]):
         ans += 1
 
 print(min(ans, len(s)-ans))
 
-# 14:40 - 15:00
-# （解説を聞いた）16:54 - 17:56
+# 10:54 - 11:42
