d34a0d7297b01bebd9ec392a787453741e3af30b
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-13 20:13:11 +0900

    fix ABC122A

--- ABC_A/ABC122A.py
+++ ABC_A/ABC122A.py
@@ -2,21 +2,9 @@
 # A - Double Helix
 # https://atcoder.jp/contests/abc122/tasks/abc122_a
 
-l = str.maketrans('AGCT', 'TCGA')
-print(input().translate(l))
+l = str.maketrans('ACGT', 'TGCA')
+b = input().translate(l)
+print(b)
 
-'''
-b = input()
-
-if b == 'A':
-    print('T')
-elif b == 'T':
-    print('A')
-elif b == 'G':
-    print('C')
-else:
-    print('G')
-'''
-
-# 11:46 - 11:49
-# （解答を閲覧）- 11:54
+# 19:37 - 19:44（解答を見た）
+# （再挑戦）20:10 - 20:12
