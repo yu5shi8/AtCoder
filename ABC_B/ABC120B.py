0a3648db346cdf718b89cf8e753a4ea2a6b166f1
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-14 21:56:56 +0900

    fix ABC120B

--- ABC_B/ABC120B.py
+++ ABC_B/ABC120B.py
@@ -1,29 +1,14 @@
 # -*- coding: utf-8 -*-
 # B - K-th Common Divisor
 # https://atcoder.jp/contests/abc120/tasks/abc120_b
-# 15:02 - 15:46
 
 a, b, k = map(int, input().split())
-ab = []
+l = []
 
-if a >= b:
-    i = b
-else:
-    i = a
-
-while i > 0:
+for i in range(max(a, b)+1, 0, -1):
     if a % i == 0 and b % i == 0:
-        ab.append(i)
-    i -= 1
+        l.append(i)
 
-print(ab[k - 1])
+print(l[k-1])
 
-# 解説を見ての別解
-# a, b, k = map(int, input().split())
-# 
-# for i in range(min(a, b), 0, -1):
-#     if a % i == 0 and b % i == 0:
-#         k -= 1
-#         if k == 0:
-#             print(i)
-#             break
+# 21:46 - 21:56
