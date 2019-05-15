ef4e7f82d81e2bddc51aa4000d4ac0400868ffee
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-15 20:46:16 +0900

    fix ABC119A

--- ABC_A/ABC119A.py
+++ ABC_A/ABC119A.py
@@ -2,9 +2,12 @@
 # A - Still TBD
 # https://atcoder.jp/contests/abc119/tasks/abc119_a
 
-s = input().replace('/', '')
+s = input()
+s = s.replace('/','')
 
-if int(s) <= 20190430:
+if s <= '20190430':
     print('Heisei')
 else:
     print('TBD')
+
+# 20:42 - 20:45
