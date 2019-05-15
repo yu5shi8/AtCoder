4235aab7cb43e521847630eb1533904af083ad8c
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-15 21:20:39 +0900

    fix ABC119B

--- ABC_B/ABC119B.py
+++ ABC_B/ABC119B.py
@@ -2,37 +2,18 @@
 # B - Digital Gifts
 # https://atcoder.jp/contests/abc119/tasks/abc119_b
 
-#19:53 - 20:02
-N = int(input())
-rate = 380000.0
-ans = 0.0
+n = int(input())
+xu = [list(input().split()) for _ in range(n)]
+btc = 380000.0
+ans = float(0)
 
-for i in range(N):
-    x, u = input().split()
-    x = float(x)
-    ans += x * rate if u =='BTC' else x
+for i in range(n):
+    count = 0
+    if xu[i][1] == 'JPY':
+        ans += int(xu[i][0])
+    else:
+        ans += btc * float(xu[i][0])
 
 print('{:.7f}'.format(ans))
 
-
-# 16:14 - 16:59
-'''
-# WA
-N = int(input())
-n = {}
-ans = 0
-
-while N > 0:
-    key_and_value = input()
-    key, value = key_and_value.split()
-    n[key] = value
-    N -= 1
-
-for i in n:
-    if n[i] == 'JPY':
-        ans += int(i)
-    else:
-        ans += 380000 * float(i)
-
-print(ans)
-'''
+# 20:52 - 21:19
