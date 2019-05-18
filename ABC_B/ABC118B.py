d3c1211621aa10d26af6cb65fc487d0cec2b7280
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-19 01:27:15 +0900

    fix ABC118B

--- ABC_B/ABC118B.py
+++ ABC_B/ABC118B.py
@@ -4,10 +4,13 @@
 
 n, m = map(int, input().split())
 ka = [list(map(int, input().split())) for i in range(n)]
-l = [0] * m
+ans = [0] * m
 
 for i in range(n):
     for j in ka[i][1:]:
-        l[j - 1] += 1
+        ans[j-1] += 1
 
-print(l.count(n))
+print(ans.count(n))
+
+# 0:54 - 1:21
+#（回答の調整）- 1:26
