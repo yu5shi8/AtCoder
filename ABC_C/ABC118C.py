80ee7dffc8cf0b1f189850c7d299a89765d0546d
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-20 21:16:43 +0900

    fix ABC118C

--- ABC_C/ABC118C.py
+++ ABC_C/ABC118C.py
@@ -16,27 +16,4 @@ for i in range(n):
 
 print(num)
 
-
-'''
-# WA
-n = int(input())
-a = list(map(int, input().split()))
-a = sorted(a)
-
-comp = [10**9] * n
-max_hp = a[n-1]
-
-for i in range(1, n):
-    if a[i] % a[i-1] == 0:
-        comp[i-1] = a[i-1]
-    else:
-        comp[i-1] = a[i] % a[i-1]
-
-min_attack = min(comp)
-ans = min(min_attack, max_hp%min_attack if max_hp%min_attack!=0 else max_hp)
-
-print(ans)
-'''
-
-# 11:06 - 12:28（WA）
-# （解説・解答を閲覧）- 12:52
+# 21:11 - 21:15
