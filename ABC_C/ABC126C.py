5713c81ea02ade986467b397541737f3a73d3612
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-20 21:07:49 +0900

    fix ABC126C

--- ABC_C/ABC126C.py
+++ ABC_C/ABC126C.py
@@ -2,30 +2,21 @@
 # C - Dice and Coin
 # https://atcoder.jp/contests/abc126/tasks/abc126_c
 
-from fractions import Fraction
-
 n, k = map(int, input().split())
-count = [0] * n
 ans = 0
 
 for i in range(1, n+1):
-    j = i
-    num = k - i
-    while num >= j:
-        j *= 2
-        count[i-1] += 1
-
-all_pattern = [0] * n
-for i in range(n):
-    all_pattern[i] = Fraction(1, n*(2**count[i]))
-
-all_pattern = sum(all_pattern)
-frac_ans = Fraction(all_pattern)
-ans = frac_ans.numerator / frac_ans.denominator
+    count = 0
+    if i >= k:
+        ans += 1 / n
+    else:
+        count = 1
+        while i * 2**count < k:
+            count += 1
+        num = (1/n) * (0.5**count)
+        ans += num
 
 print('{:.12f}'.format(ans))
 
-# 0.999986250000
-# 0.999973749998
-
-# 21:21 - 22:40（WA）
+# 20:32 - 20:48（WA）
+# （解答を閲覧）- 21:06
