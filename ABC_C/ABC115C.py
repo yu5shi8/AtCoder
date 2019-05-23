d4b89ecefb8c8b199f398deccd52bed45daa8e26
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-23 16:42:46 +0900

    fix ABC115C

--- ABC_C/ABC115C.py
+++ ABC_C/ABC115C.py
@@ -3,11 +3,14 @@
 # https://atcoder.jp/contests/abc115/tasks/abc115_c
 
 n, k = map(int, input().split())
-h = sorted([int(input()) for _ in range(n)])
+h = sorted(list(int(input()) for _ in range(n)))
+ans = []
 
-ans = min(h[i+k-1] - h[i] for i in range(n-k+1))
+for i in range(n-k+1):
+    ans.append(h[i+k-1] - h[i])
 
-print(ans)
+print(min(ans))
 
 # 10:24 - 11:35（WA）
-# （解説・解答を閲覧）- 11:48
+#（解説・解答を閲覧）- 11:48
+#（穴埋め）16:33 - 16:42
