9d40ce07eb2bc348e9c67d03cb6bba51ab03358d
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-23 10:14:27 +0900

    fix ABC115A

--- ABC_A/ABC115A.py
+++ ABC_A/ABC115A.py
@@ -3,9 +3,13 @@
 # https://atcoder.jp/contests/abc115/tasks/abc115_a
 
 d = int(input())
+count = 25
 
 if d == 25:
     print('Christmas')
 else:
-    count = 25 - d
+    count -= d
     print('Christmas' + ' Eve' * count)
+
+# 10:06 - 10:09 - 10:11
+
