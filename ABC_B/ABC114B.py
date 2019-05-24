9b7070770d5e34bd15718bda12ae249942b2ed90
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-24 16:21:40 +0900

    fix ABC114B

--- ABC_B/ABC114B.py
+++ ABC_B/ABC114B.py
@@ -3,12 +3,13 @@
 # https://atcoder.jp/contests/abc114/tasks/abc114_b
 
 s = input()
-master = 753
+temp = 753
 ans = []
 
-for i in range(len(s)):
-    x = int(s[i:i+3])
-    calc = abs(master - x)
-    ans.append(calc)
+for i in range(3, len(s)+1):
+    ans.append(abs(int(s[i-3:i]) - temp))
 
 print(min(ans))
+
+
+# 16:08 - 16:20
