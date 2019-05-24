4f0c270468ea89f2983f9fe2c0c348806a02dd56
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-24 16:00:17 +0900

    fix ABC115C

--- ABC_C/ABC115C.py
+++ ABC_C/ABC115C.py
@@ -7,10 +7,8 @@ h = sorted(list(int(input()) for _ in range(n)))
 ans = []
 
 for i in range(n-k+1):
-    ans.append(h[i+k-1] - h[i])
+    ans.append(h[i+k-1]-h[i])
 
 print(min(ans))
 
-# 10:24 - 11:35（WA）
-#（解説・解答を閲覧）- 11:48
-#（穴埋め）16:33 - 16:42
+# 15:50 - 15:59
