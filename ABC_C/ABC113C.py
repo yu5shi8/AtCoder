49c897c718481275bbbc4db4fef4559da40b8e80
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-27 15:55:28 +0900

    fix ABC113C

--- ABC_C/ABC113C.py
+++ ABC_C/ABC113C.py
@@ -4,7 +4,7 @@
 
 n, m = map(int, input().split())
 py = [[i] + list(map(int, input().split())) for i in range(m)]
-py.sort(key=lambda x:(x[1], x[2]))
+py.sort(key=lambda x:(x[1],x[2]))
 
 city = py[0][1]
 num = 0
@@ -17,11 +17,10 @@ for i in py:
         city = i[1]
     i.append(num)
 
-py.sort()
+py.sort(key=lambda x:[x[0]])
 
 for i in py:
     print('{:06}{:06}'.format(i[1], i[3]))
 
-# 17:29 - 18:23
-#（解答を閲覧）23:25 - 23:47
-#（再挑戦）23:51 - 24:05（RE）- 24:07（RE） - 24:09（AC）
+# 15:42 - 15:54
+
