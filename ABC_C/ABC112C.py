7eaf64dc5ccf972c4ad9384176140ed311b12092
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-28 22:42:10 +0900

    fix ABC112C

--- ABC_C/ABC112C.py
+++ ABC_C/ABC112C.py
@@ -3,14 +3,14 @@
 # https://atcoder.jp/contests/abc112/tasks/abc112_c
 
 n = int(input())
-xyh = [list(map(int, input().split())) for _ in range(n)]
-xyh.sort(key=lambda x: -x[2])
+xyh = [list(map(int, input().split())) for i in range(n)]
+xyh.sort(key=lambda x:-x[2])
 
 for cx in range(101):
     for cy in range(101):
         ch = None
         for x, y, h in xyh:
-            t = h + abs(cx - x) + abs(cy - y)
+            t = h + abs(x-cx) + abs(y-cy)
             if h > 0:
                 if ch is not None and t != ch:
                     break
@@ -22,8 +22,4 @@ for cx in range(101):
             print(cx, cy, ch)
             exit()
 
-# 16:11 - 17:02
-#（解説・解答を閲覧）- 17:46
-#（穴埋め解答）- 17:55
-#（再挑戦）18:21 - 18:39
-#（再々挑戦）18:39 - 18:45（WA）- 18:47（AC）
+# 22:28 - 22:41
