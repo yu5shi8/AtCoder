984d14830a6234df28452735e087e0c8e6a2e116
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-05-31 18:08:09 +0900

    fix ABC128C

--- ABC_C/ABC128C.py
+++ ABC_C/ABC128C.py
@@ -9,18 +9,17 @@ p = list(map(int, input().split()))
 ans = 0
 
 for i in range(2**n):
-    pika = 0
+    light = 0
     for j in range(m):
         count = 0
         for k in range(1, ks[j][0]+1):
             if i & 2**(ks[j][k]-1) == 2**(ks[j][k]-1):
                 count += 1
         if count % 2 == p[j]:
-            pika += 1
-    if pika == m:
+            light += 1
+    if light == m:
         ans += 1
 
 print(ans)
 
-# 16:45 - 
-# 21:08 - 22:24
+# 15:38 - 18:06
