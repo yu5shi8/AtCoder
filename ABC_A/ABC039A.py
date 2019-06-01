014596e6abe0d9bce920d8189ce9776a95e63630
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-01 20:44:55 +0900

    add ABC039A

---
+++ ABC_A/ABC039A.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# A - 高橋直体
+# https://atcoder.jp/contests/abc039/tasks/abc039_a
+
+a, b, c = map(int, input().split())
+ans = a*b*2 + b*c*2 + a*c*2
+print(ans)
+
+# 20:41 - 20:44
