b4b908d2658deed594d9aefc3b45361db2026395
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-01 20:48:46 +0900

    add ABC038A

---
+++ ABC_A/ABC038A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - お茶
+# https://atcoder.jp/contests/abc038/tasks/abc038_a
+
+s = input()
+
+if s[-1] == 'T':
+    print('YES')
+else:
+    print('NO')
+
+# 20:46 - 20:48
