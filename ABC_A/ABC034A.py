7e1deb3e2968ab1e95e067619e47be51b2d1052b
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-02 20:25:15 +0900

    add ABC034A

---
+++ ABC_A/ABC034A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - テスト
+# https://atcoder.jp/contests/abc034/tasks/abc034_a
+
+x, y = map(int, input().split())
+
+if x < y:
+    print('Better')
+else:
+    print('Worse')
+
+# 20:22 - 20:23
