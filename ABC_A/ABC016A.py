1519ea0ddc596a96ec5e2f20865b2626881212b9
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-06 10:32:00 +0900

    add ABC016A

---
+++ ABC_A/ABC016A.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# A - 12月6日
+# https://atcoder.jp/contests/abc016/tasks/abc016_1
+
+m, d = map(int, input().split())
+
+if m % d == 0:
+    print('YES')
+else:
+    print('NO')
+
+# 10:29 - 10:31
+
