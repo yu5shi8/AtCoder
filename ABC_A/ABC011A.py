7ffb067a5328c66b4fca347ec90fd91b6f6377cd
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-06 10:53:59 +0900

    add ABC011A

---
+++ ABC_A/ABC011A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - 来月は何月？
+# https://atcoder.jp/contests/abc011/tasks/abc011_1
+
+n = int(input())
+
+if n + 1 > 12:
+    print(1)
+else:
+    print(n + 1)
+
+# 10:51 - 10:53
