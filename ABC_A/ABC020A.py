b931b8956f5f00ca922d113eb8ef9cd8b7972ffc
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-06 10:02:29 +0900

    add ABC020A

---
+++ ABC_A/ABC020A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - クイズ
+# https://atcoder.jp/contests/abc020/tasks/abc020_a
+
+q = int(input())
+
+if q == 1:
+    print('ABC')
+else:
+    print('chokudai')
+
+# 10:00 - 10:02
