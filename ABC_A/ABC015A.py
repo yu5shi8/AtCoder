ec9ead2274c89c10da09fa47ca052a217a06cb83
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-06 10:34:52 +0900

    add ABC015A

---
+++ ABC_A/ABC015A.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# A - 高橋くんの研修
+# https://atcoder.jp/contests/abc015/tasks/abc015_1
+
+a = input()
+b = input()
+
+if len(a) > len(b):
+    print(a)
+else:
+    print(b)
+
+# 10:32 - 10:34
