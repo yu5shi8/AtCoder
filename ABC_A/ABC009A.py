4b54c599a73995d9ded34a7630d4a1dc1e516712
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-06 10:58:25 +0900

    add ABC009A

---
+++ ABC_A/ABC009A.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# A - 引越し作業
+# https://atcoder.jp/contests/abc009/tasks/abc009_1
+
+n = int(input())
+
+if n % 2 != 0:
+    ans = n // 2 + 1
+else:
+    ans = n // 2
+
+print(ans)
+
+# 10:56 - 10:57
