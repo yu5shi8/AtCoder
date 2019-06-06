25e9a9d672e0201674ce41c7dc58ad9066f8349b
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-06 10:46:48 +0900

    add ABC013A

---
+++ ABC_A/ABC013A.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# A - A
+# https://atcoder.jp/contests/abc013/tasks/abc013_1
+
+# 提出した解答
+x = input()
+
+if x == 'A':
+    print(1)
+elif x == 'B':
+    print(2)
+elif x == 'C':
+    print(3)
+elif x == 'D':
+    print(4)
+elif x == 'E':
+    print(5)
+
+'''
+# 覚えておきたい別解
+x = input()
+print(ord(x) - 64)
+# ord() ： その文字の Unicode コードポイントを表す整数を返す
+'''
+
+# 10:40 - 10:46
