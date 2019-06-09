ed27c390fe195c107c70149624615e8e681bfabc
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-09 23:32:10 +0900

    add ABC129A

---
+++ ABC_A/ABC129A.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# A - Airplane
+# https://atcoder.jp/contests/abc129/tasks/abc129_a
+
+pqr = list(map(int, input().split()))
+ans = sum(pqr) - max(pqr)
+print(ans)
+
+# 21:00 - 21:02
