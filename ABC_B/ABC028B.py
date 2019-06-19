9185f58f93d60eb43b2a7e95df958e0e5f9c5bb3
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-19 22:36:14 +0900

    add ABC028B

--- ABC_B/ABC028B.py
+++ ABC_B/ABC028B.py
@@ -2,13 +2,23 @@
 # B - 文字数カウント
 # https://atcoder.jp/contests/abc028/tasks/abc028_b
 
-s = list(input())
-l = ['A', 'B', 'C', 'D', 'E', 'F']
+s = input()
 ans = [0] * 6
 
-for i in range(6):
-    ans[i] = s.count(l[i])
+for i in range(len(s)):
+    if s[i] == 'A':
+        ans[0] += 1
+    elif s[i] == 'B':
+        ans[1] += 1
+    elif s[i] == 'C':
+        ans[2] += 1
+    elif s[i] == 'D':
+        ans[3] += 1
+    elif s[i] == 'E':
+        ans[4] += 1
+    elif s[i] == 'F':
+        ans[5] += 1
 
 print(*ans, sep=' ')
 
-# 18:15 - 18:29
+# 22:28 - 22:32
