839069d004d9285f432e5cf4e98cb8b563bca8d7
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-22 00:33:46 +0900

    add ABC034B

---
+++ ABC_B/ABC034B.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# B - ペア
+# https://atcoder.jp/contests/abc034/tasks/abc034_b
+
+n = int(input())
+
+if n % 2 == 0:
+    print(n - 1)
+else:
+    print(n + 1)
+
+# 0:32 - 0:33
