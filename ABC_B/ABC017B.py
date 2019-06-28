1e21ac2035b662d13958c43032e3cb80e6d9892c
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-28 23:05:15 +0900

    add ABC017B

---
+++ ABC_B/ABC017B.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# B - choku語
+# https://atcoder.jp/contests/abc017/tasks/abc017_2
+
+x = input().replace('ch', '').replace('o', '').replace('k', '').replace('u', '')
+
+if x == '':
+    print('YES')
+else:
+    print('NO')
+
+# 22:54 - 23:03
