82e2f8efeb95329cf2d076f8b3a755815f72f4b6
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-06-29 15:48:29 +0900

    fix ABC103C

--- ABC_C/ABC103C.py
+++ ABC_C/ABC103C.py
@@ -2,24 +2,11 @@
 # C - Modulo Summation
 # https://atcoder.jp/contests/abc103/tasks/abc103_c
 
-import fractions
-from functools import reduce
-
 n = int(input())
 a = list(map(int, input().split()))
-ans = 0
-
-def lcm_base(x, y):
-    return (x * y) // fractions.gcd(x, y)
-
-def lcm(*numbers):
-    return reduce(lcm_base, numbers, 1)
-
-num = lcm(*a) - 1
-
-for i in range(n):
-    ans += num % a[i]
+ans = sum(a) - n
 
 print(ans)
 
 # 15:14 - 15:41（AC）
+#（解答を見てAC）- 15:46
