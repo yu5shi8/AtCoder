cef9c41e0e91b6d4b743f67c1a4e7ce83d039058
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-07-19 16:23:36 +0900

    add ABC002B

---
+++ ABC_B/ABC002B.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# B - 罠
+# https://atcoder.jp/contests/abc002/tasks/abc002_2
+
+w = input().replace('a', '').replace('i', '').replace('u', '').replace('e', '').replace('o', '')
+print(w)
+
+# 16:19 - 16:21（AC）
