99766714bcf4b7a6be7a5668b65f424609b37d12
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-08-06 16:22:44 +0900

    add ABC005B

---
+++ ABC_B/ABC005B.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# B - おいしいたこ焼きの食べ方
+# https://atcoder.jp/contests/abc005/tasks/abc005_2
+
+n = int(input())
+t = [int(input()) for i in range(n)]
+
+print(min(t))
+
+# 22:32 - 22:34（AC）
