9d662a21224a086f5ef0bdb00316df59896f3121
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-08-15 21:39:44 +0900

    add ABC004B

---
+++ ABC_B/ABC004B.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# B - 回転
+# https://atcoder.jp/contests/abc004/tasks/abc004_2
+
+c_0 = list(input().split())
+c_1 = list(input().split())
+c_2 = list(input().split())
+c_3 = list(input().split())
+
+c_0.reverse()
+c_1.reverse()
+c_2.reverse()
+c_3.reverse()
+
+print(' '.join(c_3))
+print(' '.join(c_2))
+print(' '.join(c_1))
+print(' '.join(c_0))
+
+# 21:26 - 21:38（AC）
