5c56b63b28389891d4f62beb84ec3343a75d8ea8
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-08-19 17:41:01 +0900

    add ABC020B

---
+++ ABC_B/ABC020B.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# B - 足し算
+# https://atcoder.jp/contests/abc020/tasks/abc020_b
+
+a, b = input().split()
+
+add = a + b
+ans = int(add) * 2
+
+print(ans)
+
+# 17:38 - 17:40（AC）
