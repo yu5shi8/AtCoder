55912a5c835e6d99643c423a6cdd4f03520d17e0
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-09-05 08:40:59 +0900

    add ABC056C

---
+++ ABC_C/ABC056C.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# C - Go Home
+# https://atcoder.jp/contests/abc056/tasks/arc070_a
+
+X = int(input())
+x = 0
+t = 0
+
+while True:
+    t += 1
+    x += t
+    if x >= X:
+        break
+
+print(t)
+
+# [1] 21:55 - 22:40（解説を閲覧）- （解答を閲覧）22:49
+# [2] 8:02 - 
