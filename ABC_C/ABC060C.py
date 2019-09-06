398f18be7992d55fb596e25186430b7372f44c3d
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-09-06 11:40:31 +0900

    add ABC060C

---
+++ ABC_C/ABC060C.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# C - Sentou
+# https://atcoder.jp/contests/abc060/tasks/arc073_a
+
+N, T = map(int, input().split())
+t = list(map(int, input().split()))
+X = T
+
+for i in range(1, N):
+    if t[i] - t[i-1] <= T:
+        X += t[i] - t[i-1]
+    else:
+        X += T
+
+print(X)
+
+# 11:22 - 11:36（AC）
+
+'''
+ABC060 C - Sentou、AC🙌
+☀️ Difficulty：-1023
+⏰ 解答時間：○分
+'''
