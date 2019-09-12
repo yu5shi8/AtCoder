b9d20b1fca25db98af281655fd39cc9f544c310b
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-09-12 14:35:16 +0900

    add ABC086C

--- ABC_C/ABC086C.py
+++ ABC_C/ABC086C.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
-# ABC086C - Traveling
-# https://atcoder.jp/contests/abs/tasks/arc089_a
+# C - Traveling
+# https://atcoder.jp/contests/abc086/tasks/arc089_a
 
-n = int(input())
+N = int(input())
+T = [0]
+X = [0]
+Y = [0]
 
-for i in range(n):
+for i in range(N):
     t, x, y = map(int, input().split())
-    if (x + y) > t or (t + x + y) % 2:
+    T.append(t)
+    X.append(x)
+    Y.append(y)
+
+for i in range(1, N+1):
+    if abs(T[i-1] - T[i]) < abs(X[i-1] - X[i]) + abs(Y[i-1] - Y[i]):
         print('No')
         exit()
+    else:
+        if T[i] % 2 != 0 and (X[i]+Y[i]) % 2 == 0:
+            print('No')
+            exit()
+        elif T[i] % 2 == 0 and (X[i]+Y[i]) % 2 != 0:
+            print('No')
+            exit()
+
 print('Yes')
 
+# 13:41 - 14:18（AC）
