c44ebaeccbf205257c42259f27534556415e6d0d
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-11-04 17:48:13 +0900

    add ARC045A

---
+++ ARC_A/ARC045A.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# A - スペース高橋君
+# https://atcoder.jp/contests/arc045/tasks/arc045_a
+
+S = input().replace('Left', '<').replace('Right', '>').replace('AtCoder', 'A')
+print(S)
+
+# 17:45 - 17:46（AC）
