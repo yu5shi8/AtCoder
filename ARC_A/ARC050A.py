bc5268570d2337d31bdbf90dece7a636952c5e1e
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-11-18 19:54:18 +0900

    add ARC050A

---
+++ ARC_A/ARC050A.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# A - 大文字と小文字
+# https://atcoder.jp/contests/arc050/tasks/arc050_a
+
+C, c = input().split()
+check = c.upper()
+
+if C == check:
+    print('Yes')
+else:
+    print('No')
+
+# 19:48 - 19:52（AC）
