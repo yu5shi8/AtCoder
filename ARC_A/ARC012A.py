7dc978280e1e8f8b5efc027dd703508e028310d4
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-11-23 20:17:09 +0900

    add ARC012A

---
+++ ARC_A/ARC012A.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# A - 週末
+# https://atcoder.jp/contests/arc012/tasks/arc012_1
+
+day = input()
+week = {'Sunday'    : 0, 
+        'Monday'    : 5, 
+        'Tuesday'   : 4, 
+        'Wednesday' : 3, 
+        'Thursday'  : 2, 
+        'Friday'    : 1, 
+        'Saturday'  : 0}
+
+print(week[day])
+
+# 20:11 - 20:16（AC）
