c7cc98332b40242d18a2c75ae4d4f0ad964819d0
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2019-11-23 20:21:03 +0900

    add ARC014A

---
+++ ARC_A/ARC014A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - 君が望むなら世界中全てのたこ焼きを赤と青に染め上げよう
+# https://atcoder.jp/contests/arc014/tasks/arc014_1
+
+N = int(input())
+
+if N % 2 == 1:
+    print('Red')
+else:
+    print('Blue')
+
+# 20:18 - 20:20（AC）
