790a8dad6aa37b1c349c28d3b253ff74e88a8a5e
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2020-02-14 22:53:02 +0900

    add ABC151A

---
+++ ABC_A/ABC151A.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# A - Next Alphabet
+# https://atcoder.jp/contests/abc151/tasks/abc151_a
+
+# C = input()
+
+# for i in range(1, 27):
+#     if C == chr(i + 96):
+#         ans = chr(i + 97)
+#         print(ans)
+#         exit()
+
+C = input()
+ans = ord(C)+1
+print(chr(ans))
+
+# 22:42 - 22:47（AC）- 22:52
+
+'''
+ABC151A - Next Alphabet、自力AC🙌
+あ゛ーーー chr() は思い出せたのに、ord() が思い出せなかった！ くやしー
+🙂 Difficulty：0 / Median Solve Time：4分
+⏰ 解答時間：5分
+https://atcoder.jp/contests/abc151/submissions/10091468
+'''
