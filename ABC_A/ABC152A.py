0df1f65e7723ace6668aa37927607f38d39cbf3c
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2020-02-15 23:32:08 +0900

    add ABC152A

---
+++ ABC_A/ABC152A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - AC or WA
+# https://atcoder.jp/contests/abc152/tasks/abc152_a
+
+N, M = map(int, input().split())
+
+if N == M:
+    print('Yes')
+else:
+    print('No')
+
+# 23:29 - 23:31（AC）
