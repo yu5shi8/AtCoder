cc474d507a6e2b0f5feaee76ad794038ff67d8e0
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2020-03-08 00:14:04 +0900

    add ABC157A

---
+++ ABC_A/ABC157A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - Duplex Printing
+# https://atcoder.jp/contests/abc157/tasks/abc157_a
+
+N = int(input())
+
+if N % 2 == 0:
+    print(N // 2)
+else:
+    print(N // 2 + 1)
+
+# 20:50 - 20:51（AC）
