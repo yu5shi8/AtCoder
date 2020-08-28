23889e94a835abf51d56e550598cbc9c6b5eb0d6
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2020-08-28 22:34:15 +0900

    add ABC174A

---
+++ ABC_A/ABC174A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - Air Conditioner
+# https://atcoder.jp/contests/abc174/tasks/abc174_a
+
+X = int(input())
+
+if X >= 30:
+    print('Yes')
+else:
+    print('No')
+
+# 14:44 - 14:45（AC）
