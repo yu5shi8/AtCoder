eeaa51d6b0b5c22a2028bec4ef3b4e9bb166c8a5
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2020-09-29 15:20:38 +0900

    add ABC164C

---
+++ ABC_C/ABC164C.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# C - gacha
+# https://atcoder.jp/contests/abc164/tasks/abc164_c
+
+N = int(input())
+S = [input() for _ in range(N)]
+
+print(len(set(S)))
+
+# 15:17 - 15:19（AC）
