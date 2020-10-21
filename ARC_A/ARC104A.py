b290a0613d0d6b8f3d34e808388d93bcefe9eb6f
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2020-10-22 01:08:14 +0900

    add ARC104A

---
+++ ARC_A/ARC104A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# A - Plus Minus
+# https://atcoder.jp/contests/arc104/tasks/arc104_a
+
+A, B = map(int, input().split())
+
+Y = (A - B) // 2
+X = A - Y
+
+print(X, Y)
+
+# 1:03 - 1:07（AC）
