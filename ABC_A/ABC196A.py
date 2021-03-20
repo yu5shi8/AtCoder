f9ca69001ece8379e3a70c993c44b540f8be2217
yu5shi8 <royal_unicorn411@hotmail.co.jp>
2021-03-20 23:04:03 +0900

    ABC196

---
+++ ABC_A/ABC196A.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# 
+# 
+
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+calc1 = a - d
+calc2 = b - c
+
+print(max(calc1, calc2))
+
+# 21:00 - 21:08（AC）
---
+++ ABC_B/ABC196B.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 
+# 
+
+from decimal import Decimal
+
+X = input()
+print(int(Decimal(X)))
+
+# 21:08 - 21:24（AC）
